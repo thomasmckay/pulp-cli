70d26212c18095ff6bb5169c7c2c94b229ca0449
werwty <bihan.zh@gmail.com>
2018-06-07 12:18:23 -0400

    Add name->pk lookup support

--- pulpcli/main.py
+++ pulpcli/main.py
@@ -6,6 +6,7 @@ import urllib.parse as urlparse
 
 from progress.spinner import Spinner
 from time import sleep
+from uuid import UUID
 
 
 DOCUMENT_PATH = "/home/vagrant/.coreapi/document.json"
@@ -39,18 +40,45 @@ def client(ctx):
     click.echo(ctx.get_help())
 
 
-def apicall(*args, **kwargs):
+def is_uuid4(uuid_string):
+    try:
+        val = UUID(uuid_string, version=4)
+    except ValueError:
+        return False
+
+    return val.hex == uuid_string
+
+
+def echo_resp(response):
     codec = coreapi.codecs.DisplayCodec()
+    click.secho(codec.encode(response, colorize=True), fg="green")
+
+
+def apicall(*args, **kwargs):
 
     ctx = click.get_current_context()
     keys = ctx.command_path.split(" ")[1:]
 
-    resp = coreapi_client.action(
-        decoded_doc, keys, params={k: v for k, v in kwargs.items() if v is not None}
-    )
+    params = {k: v for k, v in kwargs.items() if v is not None}
 
-    click.echo(codec.encode(resp, colorize=True))
+    for k, v in params.items():
 
+        # If as passed in pk is not a uuid, see if we can find the uuid
+        if k.endswith("_pk") and not is_uuid4(v):
+            resp = coreapi_client.action(
+                decoded_doc, [keys[0], "list"], params={"name": v}
+            )
+            if len(resp["results"]) == 1:
+                params[k] = resp["results"][0]["id"]
+            else:
+                click.secho("Invalid id or name", fg="red")
+                return
+
+    resp = coreapi_client.action(decoded_doc, keys, params=params)
+
+    echo_resp(resp)
+
+    # If coreapi client returns task_id, follow it and run a spinner until task is complete
     if "task_id" in resp:
         spinner = Spinner("Loading ")
         task_progress = coreapi_client.action(
@@ -62,23 +90,31 @@ def apicall(*args, **kwargs):
             task_progress = coreapi_client.action(
                 decoded_doc, ["tasks", "read"], params={"id": resp.get("task_id")}
             )
+        echo_resp(task_progress)
 
+    # Pagination support
     while resp.get("next") or resp.get("previous"):
         move = click.prompt("N for next page, P for previous")
-        if move == "N":
+        if move.lower() == "n":
             url = resp.get("next")
-        else:
+        elif move.lower() == "p":
             url = resp.get("previous")
+        else:
+            break
         parsed = urlparse.urlparse(url)
-        resp = coreapi_client.action(
-            decoded_doc, keys, {"cursor": urlparse.parse_qs(parsed.query)["cursor"]}
-        )
-        click.echo(codec.encode(resp, colorize=True))
+        params["cursor"] = urlparse.parse_qs(parsed.query)["cursor"][0]
+        resp = coreapi_client.action(decoded_doc, keys, params=params)
+
+        echo_resp(resp)
 
 
 def add_command(parent_command, name, metadata):
-    if "_type" in metadata:
+
+    # _type = link denotes a end command, otherwise there is more nested commands to go through
+    if metadata.get("_type") == "link":
         options = []
+
+        # fields in coreapi denotes options
         for field in metadata.get("fields", []):
             option_name = "--" + field.get("name")
             options.append(
@@ -88,7 +124,6 @@ def add_command(parent_command, name, metadata):
                     help=field.get("schema", {}).get("description", ""),
                 )
             )
-
         command = click.Command(
             name, callback=apicall, help=metadata.get("description", ""), params=options
         )
